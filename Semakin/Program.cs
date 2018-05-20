fffb9a1ec269691d9704e90ddd21faa10f26f217
SemakinIvan <ivan.siemakin@mail.ru>
2018-05-20 19:16:13 +0300

    Первый коммит

---
+++ Semakin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semakin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+<<<<<<< HEAD
+gflhmdl.gfjdfklgkdnlgfhl;dkgl;dkgklj
+=======
+vtoraya vetka
+>>>>>>> Branch1
+        }
+    }
+}
+fdjklgdjl;jf;dj;fgjlkdf;lkjl;d
